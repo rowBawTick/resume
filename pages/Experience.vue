419e72ee5ecf3ebb1214cf7f5c97b11329931437
chris-dev <chris@iterative-edge.dev>
2025-01-16 13:34:28 +0000

    Updated the Experience.vue page

--- pages/Experience.vue
+++ pages/Experience.vue
@@ -9,16 +9,31 @@ export default {
         <h2 class="section-heading">Experience</h2>
         <ul class="mt-4 space-y-8">
             <li>
-                <h3 class="text-slate-200 font-bold">Senior Frontend Engineer</h3>
                 <p class="text-slate-400">2024 — Present</p>
+                <h3 class="text-slate-200 font-bold">Software Engineer - Iterative Edge</h3>
                 <p class="mt-2">
-                    Build and maintain UI components while advocating for accessibility and performance.
+                    Currently freelancing for a small company building out AI infrastructure in the property technology
+                    sector. Working on a few other small projects in parallel.
                 </p>
             </li>
             <li>
-                <h3 class="text-slate-200 font-bold">Frontend Developer</h3>
-                <p class="text-slate-400">2018 — 2024</p>
-                <p class="mt-2">Worked on various projects with a focus on modern web development tools.</p>
+                <p class="text-slate-400">2021 — 2024</p>
+                <h3 class="text-slate-200 font-bold">Software Engineer - Substantive Research</h3>
+                <p class="mt-2">
+                    Fostered collaboration among junior developers, improved code quality through testing and secure
+                    practices, and built a streamlined CI/CD pipeline. Engaged with clients to ensure features met their
+                    needs and delivered lasting value. Maintained a strong emphasis on secure coding practices and
+                    conducted annual penetration tests to safeguard sensitive data, ensuring industry compliance.
+                </p>
+            </li>
+            <li>
+                <h3 class="text-slate-200 font-bold">Junior Software Developer - Gigabyte Software</h3>
+                <p class="text-slate-400">2021 — 2024</p>
+                <p class="mt-2">
+                    Worked with stakeholders to enhance a bespoke enterprise system, addressing issues and adding new
+                    features. Supported greenfield projects, facilitated team communication as a part-time scrum master,
+                    and improved workflow through sprint planning and retrospectives.
+                </p>
             </li>
         </ul>
     </section>
