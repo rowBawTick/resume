a042b85bc20ee5447c19f56e2a35be0e70c37086
chris-dev <chris@iterative-edge.dev>
2025-01-16 15:19:08 +0000

    Added the Education.vue section.

--- pages/Education.vue
+++ pages/Education.vue
@@ -1,21 +1,35 @@
-<script>
-export default {
-    name: 'Education'
-}
+<script setup>
+import EducationItem from '~/components/EducationItem.vue';
+
+const schools = [
+    {
+        date: '2010 — 2012',
+        name: 'Utrecht University',
+        website: 'https://www.uu.nl/en/research/physics-and-astronomy',
+        location: 'Utrecht',
+        locationLink: 'https://www.google.co.uk/maps/place/Utrecht,+Netherlands/@52.084166,5.0124519',
+        degree: 'MSc in Astrophysics and Space Research'
+    },
+    {
+        date: '2005 — 2009',
+        name: 'University of Edinburgh',
+        website: 'https://www.ph.ed.ac.uk',
+        location: 'Edinburgh',
+        locationLink: 'https://www.google.co.uk/maps/place/Edinburgh/@55.9411885,-3.2753782',
+        degree: 'BSc with Hons in Astrophysics'
+    }
+];
 </script>
 
 <template>
     <section id="education" class="mb-16">
         <h2 class="section-heading">Education</h2>
-        <ul class="mt-4 space-y-8">
-            <li class="bg-slate-800 p-4 rounded-md">
-                <h3 class="text-slate-200 font-bold">University 1</h3>
-                <p class="mt-2">A brief description of the project.</p>
-            </li>
-            <li class="bg-slate-800 p-4 rounded-md">
-                <h3 class="text-slate-200 font-bold">University 2</h3>
-                <p class="mt-2">Another brief description.</p>
-            </li>
+        <ul class="mt-4 space-y-4">
+            <EducationItem
+                v-for="school in schools"
+                :key="school.name"
+                v-bind="school"
+            />
         </ul>
     </section>
 </template>
